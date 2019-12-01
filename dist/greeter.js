e5db25b9583ac2e61ab6eefb9df756d7b8b4add5
Alexis Adonis <acadonis@gmail.com>
2019-12-01 20:17:12 +0000

    typescript demo done

--- dist/greeter.js
+++ dist/greeter.js
@@ -1,6 +1,17 @@
 "use strict";
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = "Jane User";
+// let user = { firstName: "Jane", lastName: "User" };
+//using class of Student
+var user = new Student("Jane", "M.", "User");
 document.body.textContent = greeter(user);
